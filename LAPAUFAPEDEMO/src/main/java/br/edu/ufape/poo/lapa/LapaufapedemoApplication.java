60a894b789fb673f60a05d09317c23998fc9a646
Gustavo Ferreira Wanderley <mestreguga24@gmail.com>
2023-08-09 10:55:09 -0300

    Primeiro commit

---
+++ LAPAUFAPEDEMO/src/main/java/br/edu/ufape/poo/lapa/LapaufapedemoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.poo.lapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapaufapedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapaufapedemoApplication.class, args);
+	}
+
+}
