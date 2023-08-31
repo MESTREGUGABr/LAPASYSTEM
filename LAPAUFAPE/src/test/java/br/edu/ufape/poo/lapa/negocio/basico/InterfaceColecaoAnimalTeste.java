dee6d5d8ed716bd506c82e4ea25c549962f58f3a
NicolyLana <nicolylana.dev@gmail.com>
2023-08-31 15:17:03 -0300

    Casos de Testes Animal, ParteNecropsia e Tutor

--- LAPAUFAPE/src/test/java/br/edu/ufape/poo/lapa/negocio/basico/InterfaceColecaoAnimalTeste.java
+++ LAPAUFAPE/src/test/java/br/edu/ufape/poo/lapa/negocio/basico/InterfaceColecaoAnimalTeste.java
@@ -1,14 +1,16 @@
 package br.edu.ufape.poo.lapa.negocio.basico;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Date;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import br.edu.ufape.poo.lapa.dados.InterfaceColecaoAnimal;
+import br.edu.ufape.poo.lapa.dados.InterfaceColecaoTutor;
 import br.edu.ufape.poo.lapa.negocio.basico.Enum.Sexo;
 import br.edu.ufape.poo.lapa.negocio.basico.Enum.Especie;
 
@@ -16,24 +18,45 @@ import br.edu.ufape.poo.lapa.negocio.basico.Enum.Especie;
 public class InterfaceColecaoAnimalTeste {
 
 	@Autowired
-	private InterfaceColecaoAnimal ColecaoAnimal;
+	private InterfaceColecaoAnimal colecaoAnimal; // Corrigido o nome da variável
 	Sexo sex = Sexo.MACHO;
 	Especie esp = Especie.BOVINO;
 	
-	Tutor tut = new Tutor("Gabriel","gabriel@gmail.com","87998270096");
+	@Autowired
+	private InterfaceColecaoTutor colecaoTutor;
+
+//	@Test
+//	void cadastroAnimal() {
+//		long qtdAnimal = colecaoAnimal.count(); // Usar a variável correta aqui
+//
+//		Date obito = new Date();
+//
+//		Tutor tut = new Tutor("Gabriel", "gabriel@gmail.com", "87998270096");
+//
+//		Animal ani = new Animal("Lindinha", tut, "Pesadão", 300, 100, "NaoSeiOqEh", 3, esp, sex, 140, obito);
+//		colecaoAnimal.save(ani); // Usar a variável correta aqui
+//		long qtdAnimal1 = colecaoAnimal.count(); // Usar a variável correta aqui
+//
+//		assertEquals(qtdAnimal + 1, qtdAnimal1);
+//	}
 	
 	@Test
 	void cadastroAnimal() {
-		long qtdAnimal = ColecaoAnimal.count();
+		long qtdAnimal = colecaoAnimal.count();
 		
 		Date obito = new Date();
+
+		Long idTutorExistente = 1L;
+
+		Optional<Tutor> tutorOptional = colecaoTutor.findById(idTutorExistente);
+		assertTrue(tutorOptional.isPresent(), "Tutor não encontrado");
+
+		Tutor tutorExistente = tutorOptional.get();
+
+		Animal ani = new Animal("Lindinha", tutorExistente, "Pesadão", 300, 100, "NaoSeiOqEh", 3, esp, sex, 140, obito);
+		colecaoAnimal.save(ani);
+		long qtdAnimal1 = colecaoAnimal.count();
 		
-		Tutor tut = new Tutor("Gabriel","gabriel@gmail.com","87998270096");
-		
-		Animal ani = new Animal("Lindinha", tut, "Pesadão", 300, 100, "NaoSeiOqEh", 3, esp, sex, 140, obito);
-		ColecaoAnimal.save(ani);
-		long qtdAnimal1 = ColecaoAnimal.count();
-		
-		assertEquals(qtdAnimal+1, qtdAnimal1);
+		assertEquals(qtdAnimal + 1, qtdAnimal1);
 	}
 }
--- LAPAUFAPE/src/test/java/br/edu/ufape/poo/lapa/negocio/basico/InterfaceColecaoParteNecropsiaTeste.java
+++ LAPAUFAPE/src/test/java/br/edu/ufape/poo/lapa/negocio/basico/InterfaceColecaoParteNecropsiaTeste.java
@@ -6,21 +6,47 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import br.edu.ufape.poo.lapa.dados.InterfaceColecaoOrgao;
 import br.edu.ufape.poo.lapa.dados.InterfaceColecaoParteNecropsia;
 
 @SpringBootTest
 public class InterfaceColecaoParteNecropsiaTeste {
 
 	@Autowired
-	private InterfaceColecaoParteNecropsia ColecaoParteNecropsia;
+	private InterfaceColecaoParteNecropsia colecaoParteNecropsia;
+	
+	@Autowired
+	private InterfaceColecaoOrgao colecaoOrgao;
+	
+//	@Test
+//	void cadastroParteNecropsia() {
+//		long qtdParteNecropsia= colecaoParteNecropsia.count();
+//		ParteNecropsia part = new ParteNecropsia("Caixa Toracica");
+//		colecaoParteNecropsia.save(part);
+//		long qtdParteNecropsia1 = colecaoParteNecropsia.count();
+//		
+//		assertEquals(qtdParteNecropsia+1, qtdParteNecropsia1);
+//	}
 	
 	@Test
-	void cadastroParteNecropsia() {
-		long qtdParteNecropsia= ColecaoParteNecropsia.count();
-		ParteNecropsia part = new ParteNecropsia("Caixa Toracica");
-		ColecaoParteNecropsia.save(part);
-		long qtdParteNecropsia1 = ColecaoParteNecropsia.count();
-		
-		assertEquals(qtdParteNecropsia+1, qtdParteNecropsia1);
+	void testCadastroParteNecropsiaComOrgaoValido() {
+	    long qtdParteNecropsia = colecaoParteNecropsia.count();
+
+	    Long orgaoId = 1L;
+
+	    Orgao orgao = colecaoOrgao.findById(orgaoId).orElse(null);
+
+	    if (orgao == null) {
+	        return;
+	    }
+
+	    ParteNecropsia part = new ParteNecropsia("Caixa Test");
+	    part.setOrgao(orgao);
+
+	    colecaoParteNecropsia.save(part);
+
+	    long qtdParteNecropsia1 = colecaoParteNecropsia.count();
+
+	    assertEquals(qtdParteNecropsia + 1, qtdParteNecropsia1);
 	}
 }
